75a85246808b82870989ff6b2fd1a208bf24a98b
AryaF07 <AryaFarzanehnia@outlook.com>
2025-10-09 14:54:13 +0100

    Created a Move structure consisting of the piece and their current index in the chessboard and their index after the move
    Created a findmoves function that returns a list of moves for all pieces in the board in that current state
    
    Created a new  move function that takes the move structure as a parameter
    
    Started implementing the minimax algorithm

--- Bot.cs
+++ Bot.cs
@@ -54,7 +54,7 @@ namespace ChessNEA
             int randomNum2 = rand.Next(movePieces[randomNum].legalmoves.Count);
             previousRow = (movePieces[randomNum].Position.Y - 5) / 60;
             previousCol = (movePieces[randomNum].Position.X - 165) / 60;
-
+            
             col = movePieces[randomNum].legalmoves[randomNum2].X;
             row = movePieces[randomNum].legalmoves[randomNum2].Y;
             foreach (Piece piece in movePieces)
